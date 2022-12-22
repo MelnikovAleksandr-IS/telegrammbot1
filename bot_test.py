8bea66db6cac98f7d9173595294376a13ad28a9b
MelnikovAleksandr-IS <115283895+MelnikovAleksandr-IS@users.noreply.github.com>
2022-12-22 21:20:57 +0300

    Update bot_test.py

--- bot_test.py
+++ bot_test.py
@@ -1,1 +1,11 @@
 import unittest
+from bot import hmtimeswork, hmtimeshome
+
+class bot_test(unittest.TestCase):
+  def test(self):
+    self.assertEqual(hmtimeswork(12), )
+  def test1(self):
+    self.assertEqual(hmtimeshome(15, )
+                     
+if __name__ == "__main__":
+    unittest.main()
