4254a2c2cf74a498148d3272b59807a1f83ab01c
MelnikovAleksandr-IS <115283895+MelnikovAleksandr-IS@users.noreply.github.com>
2022-12-22 21:27:27 +0300

    Update bot_test.py

--- bot_test.py
+++ bot_test.py
@@ -3,9 +3,13 @@ from bot import hmtimeswork, hmtimeshome
 
 class bot_test(unittest.TestCase):
   def test(self):
-    self.assertEqual(hmtimeswork(12), )
+    self.assertEqual(hmtimeswork(12), 48)
   def test1(self):
-    self.assertEqual(hmtimeshome(15, )
+    self.assertEqual(hmtimeswork(15), 60)
+  def test2(self):
+    self.assertEqual(hmtimeshome(15), 9)
+  def test3(self):
+    self.assertEqual(hmtimeshome(10), 13)
                      
 if __name__ == "__main__":
     unittest.main()
